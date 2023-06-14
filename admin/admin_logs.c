aec6c6aad0eaf7b05565a4c7068ea72bbc877159
yujin <existarr@gmail.com>
2023-06-14 07:05:56 +0000

    updates

--- admin/admin_logs.c
+++ admin/admin_logs.c
@@ -16,68 +16,77 @@
 #define MQTT_PORT	1883
 #define MAX_TOKEN	7
 
-#define FIREBASE_PROJECT_ID "moappfinal-bbff1"
-#define FIREBASE_API_KEY "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCYGID1aNyf0Hf0\nzcwci+IZe+mLMbohoxG7730bOBzXqpMRuNqS8GQ7FieQykz8r3KN8tizFljKd5xi\nCh7ATwUOYtlf2URR80ufrg4 Sbx9gQxipNLG7yXn1bIuFBCPFKyxNnlNf4AgUXkIj\nR95181MrqvwALJs+nlkWISNgK+xcX2R0KNDeVDhIcyr/CTum3O6/p1rB9V5bl8OP\n41P/hBsWw9qu+dBAng5eLaCq1IaogRgsyYw/apM4UsAGftcT1lYnhGxqJ6OscxaT\nTJpObiLNUBCbp0YmEh3BRZGAGX2XaiietOQROKMu09GH33tmP2/DtZ4K2epfeaIc\nZ5VpWos7AgMBAAECggEAJ7pa0P+DhXEgLD8nPhuhwvNVdQjXrBqassLjJF17VzAU\ne1AUM8Z7/90RF6ZoBqbTXtWcmebP6Y7lt8wZ7TSWxwO18O3hBSDuU3bU8Sa6hNvx\n0JzVgg4l/5/LL8L9ju5wg5PEBmqP4bYfQ3Tt3FRjGKhwa6E7KdeVwIyGX4QJmLTg\nwoEMYFAgwk0vJ1X6Eb2FyK7toawOSvRN/cMnJfy1pxj1ucwvTX0eeaQ5bf6GRS5l\nvh34Nx0pV0Fup7UHIjvlgnbNh/iSRAv1ylJBnG0SvsESLu/ae89S9gYXzTITSqGE\nwS9UqAcunFGTiXeIJqa2sB4Wtj9G1JbHXt7dvD2d9QKBgQDTvutmY+0MNouDL43T\nsZ4X7KLMXZYV8MKETEiPzHWdqLq7XIV05tOLFIjw2aKzlk7dWUcJ/h4pqBbaCYcG\nSWF8XyAB2DNc62BWcM8/LnBq2md2K4fswayWHaFiTKQEO+wm7VmpmfeZim5x3mAc\nlcmFsM49G4r4+g6fye/l20oehwKBgQC34h0u4PQ5Abeo0dIL/7/rVNfDNc0DPL3U\nPZkfa5gydyQrnGQsqtxr5vwlyHoPA3Dl6fQVeWUcS1leI37q64kYd4zzZJpX7YHl\nObDl3u+XZTOlc7BGdQ0ov0i5iqGHyv8U0RD3X8zae56Ze7xJPVT8nNF82ZgweBg6\nhOu1lH1GrQKBgQDAfvFLzQ4EK1YbddDOoctiXosFlRLLcWu0kBDrQWmvYWQYzpy4\n0NuBtbJkmsXR05RwoCf3yqArKJdVspGgwncwdN4JLz5Ke+TB0J5oBtTGHzUtdZeF\nQfCfK+K6FYiKHNur69y2VbWFTRbox8vCUN26fyiO3woxHErNMuy3nuq74wKBgESh\n67yMj/sbDf6/2cNKwMpIT4P3974mlL+8AgP9N8BWQbTZWXZmi4DmpORBUHV8Fk5q\nJQVC2x9VbFS7sOpj4Ta1oHFT8luxNVNmVgVmUUzTpBQz+u5BUBkOVEmUWbgCZ3TQ\nl6GNKzu8+jRVP7CiaK8kpPgRi0FEAd8Nr6CFfMC9AoGBAKaPobWio0d6ZGX6Y+Jf\nR7dq6xeBP7tnw9AyudZVR882TOG0ik0jcatMuvsTLAXNufk7X+QLIqlG8BrSCI+m\nSPfYrbd+mhzHTHLvyVy5ke+5zCuwUaAUe+dViSw5hAz6SC5PKNkudR88qcMgDtqK\ncxFq1JiH7lWeyCk1zZShJbbi"
-#define FIREBASE_COLLECTION "logs"
+// #define FIREBASE_PROJECT_ID "moappfinal-bbff1"
+// #define FIREBASE_API_KEY "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCYGID1aNyf0Hf0\nzcwci+IZe+mLMbohoxG7730bOBzXqpMRuNqS8GQ7FieQykz8r3KN8tizFljKd5xi\nCh7ATwUOYtlf2URR80ufrg4 Sbx9gQxipNLG7yXn1bIuFBCPFKyxNnlNf4AgUXkIj\nR95181MrqvwALJs+nlkWISNgK+xcX2R0KNDeVDhIcyr/CTum3O6/p1rB9V5bl8OP\n41P/hBsWw9qu+dBAng5eLaCq1IaogRgsyYw/apM4UsAGftcT1lYnhGxqJ6OscxaT\nTJpObiLNUBCbp0YmEh3BRZGAGX2XaiietOQROKMu09GH33tmP2/DtZ4K2epfeaIc\nZ5VpWos7AgMBAAECggEAJ7pa0P+DhXEgLD8nPhuhwvNVdQjXrBqassLjJF17VzAU\ne1AUM8Z7/90RF6ZoBqbTXtWcmebP6Y7lt8wZ7TSWxwO18O3hBSDuU3bU8Sa6hNvx\n0JzVgg4l/5/LL8L9ju5wg5PEBmqP4bYfQ3Tt3FRjGKhwa6E7KdeVwIyGX4QJmLTg\nwoEMYFAgwk0vJ1X6Eb2FyK7toawOSvRN/cMnJfy1pxj1ucwvTX0eeaQ5bf6GRS5l\nvh34Nx0pV0Fup7UHIjvlgnbNh/iSRAv1ylJBnG0SvsESLu/ae89S9gYXzTITSqGE\nwS9UqAcunFGTiXeIJqa2sB4Wtj9G1JbHXt7dvD2d9QKBgQDTvutmY+0MNouDL43T\nsZ4X7KLMXZYV8MKETEiPzHWdqLq7XIV05tOLFIjw2aKzlk7dWUcJ/h4pqBbaCYcG\nSWF8XyAB2DNc62BWcM8/LnBq2md2K4fswayWHaFiTKQEO+wm7VmpmfeZim5x3mAc\nlcmFsM49G4r4+g6fye/l20oehwKBgQC34h0u4PQ5Abeo0dIL/7/rVNfDNc0DPL3U\nPZkfa5gydyQrnGQsqtxr5vwlyHoPA3Dl6fQVeWUcS1leI37q64kYd4zzZJpX7YHl\nObDl3u+XZTOlc7BGdQ0ov0i5iqGHyv8U0RD3X8zae56Ze7xJPVT8nNF82ZgweBg6\nhOu1lH1GrQKBgQDAfvFLzQ4EK1YbddDOoctiXosFlRLLcWu0kBDrQWmvYWQYzpy4\n0NuBtbJkmsXR05RwoCf3yqArKJdVspGgwncwdN4JLz5Ke+TB0J5oBtTGHzUtdZeF\nQfCfK+K6FYiKHNur69y2VbWFTRbox8vCUN26fyiO3woxHErNMuy3nuq74wKBgESh\n67yMj/sbDf6/2cNKwMpIT4P3974mlL+8AgP9N8BWQbTZWXZmi4DmpORBUHV8Fk5q\nJQVC2x9VbFS7sOpj4Ta1oHFT8luxNVNmVgVmUUzTpBQz+u5BUBkOVEmUWbgCZ3TQ\nl6GNKzu8+jRVP7CiaK8kpPgRi0FEAd8Nr6CFfMC9AoGBAKaPobWio0d6ZGX6Y+Jf\nR7dq6xeBP7tnw9AyudZVR882TOG0ik0jcatMuvsTLAXNufk7X+QLIqlG8BrSCI+m\nSPfYrbd+mhzHTHLvyVy5ke+5zCuwUaAUe+dViSw5hAz6SC5PKNkudR88qcMgDtqK\ncxFq1JiH7lWeyCk1zZShJbbi"
+// #define FIREBASE_COLLECTION "logs"
 
 //log topics (distinguish between publish messages from subscriber and publisher in a location)
 char *const topics[] = {"admin/logs/sub", "admin/logs/pub"};
 
-void write_callback(void *response, size_t size, size_t nmemb, void *user_data){
-	char *response_str = (char *)response;
-    printf("%.*s", (int)(size * nmemb), response_str);
-}
+// void write_callback(void *response, size_t size, size_t nmemb, void *user_data){
+// 	char *response_str = (char *)response;
+//     printf("%.*s", (int)(size * nmemb), response_str);
+// }
 
-void store_log(const char *tokens[]) {
-    CURL *curl;
-    CURLcode res;
-    char url[1024];
-    char request[4096];
+// void store_log(const char *tokens[]){
+// 	CURL *curl;
+// 	CURLcode res;
+// 	char url[1024];
+// 	char request[1024];
+// 	char response[4096];
+// 	char fields[7][30] = {"institution", "location", "room", "timestamp", "level", "decibel", "status"};
 
-    snprintf(url, sizeof(url), "https://firestore.googleapis.com/v1/projects/%s/databases/(default)/documents/%s", FIREBASE_PROJECT_ID, FIREBASE_COLLECTION);
+// 	snprintf(url, sizeof(url), "https://firestore.googleapis.com/v1/projects/%s/databases/(default)/documents/%s", FIREBASE_PROJECT_ID, FIREBASE_COLLECTION);
+	
+// 	strcpy(request, "{");
+// 	for(int i=0; i<7; i++){
+// 		strcat(request, "\"");
+// 		strcat(request, fields[i]);
+// 		strcat(request, "\": \"");
+// 		strcat(request, tokens[i]);
+// 		strcat(request, "\"");
+// 		if(i != 6){
+// 			strcat(request, ", ");
+// 		}
+// 	}
+// 	strcat(request, "}");
 
-    strcpy(request, "{");
-    for (int i = 0; i < 7; i++) {
-        strcat(request, "\"");
-        strcat(request, fields[i]);
-        strcat(request, "\": { \"stringValue\": \"");
-        strcat(request, tokens[i]);
-        strcat(request, "\" }");
-        if (i != 6) {
-            strcat(request, ", ");
-        }
-    }
-    strcat(request, "}");
+// 	printf("%s\n", url);
+// 	printf("%s\n", request);
 
-    curl_global_init(CURL_GLOBAL_ALL);
-    curl = curl_easy_init();
+// 	curl_global_init(CURL_GLOBAL_ALL);
+// 	curl = curl_easy_init();
 
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_POST, 1L);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request);
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_slist_append(NULL, "Content-Type: application/json"));
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+// 	response[0] = '\0';
 
-        char auth_header[4096];
-        snprintf(auth_header, sizeof(auth_header), "Authorization: Bearer %s", FIREBASE_API_KEY);
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_slist_append(NULL, auth_header));
+// 	if(curl){
+// 		curl_easy_setopt(curl, CURLOPT_URL, url);
+//         curl_easy_setopt(curl, CURLOPT_POST, 1L);
+// 		curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+//         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request);
+//         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+//         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+//         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_slist_append(NULL, "Content-Type: application/json"));
+//         curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+//         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
+//         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 
-        res = curl_easy_perform(curl);
+//         char auth_header[4096];
+//         snprintf(auth_header, sizeof(auth_header), "Authorization: Bearer %s", FIREBASE_API_KEY);
+//         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_slist_append(NULL, auth_header));
+// 		res = curl_easy_perform(curl);
 
-        if (res == CURLE_OK) {
-            printf("Data stored successfully.\n");
-        } else {
-            fprintf(stderr, "Request failed: %s\n", curl_easy_strerror(res));
-        }
+// 		if(res == CURLE_OK){
+// 			printf("data stored successfully.\n");
+// 		} else {
+// 			fprintf(stderr, "request failed: %s\n", curl_easy_strerror(res));
+// 		}
 
-        curl_easy_cleanup(curl);
-    }
+// 		curl_easy_cleanup(curl);
+// 	}
 
-    curl_global_cleanup();
-}
+// 	curl_global_cleanup();
+// }
 
 /*
  * This function reconnects to broker when the connection is disconnected.
@@ -173,7 +182,7 @@ void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_messag
 	}
 
 	//store log
-	store_log((const char**)tokens);
+	// store_log((const char**)tokens);
     
 	//print out the log message
 	printf("[%s] location: %s_%s_%s, decibel: %s, noise_level: %s, health_status: %s, time: %s\n", msg->topic, tokens[0], tokens[1], tokens[2], tokens[5], tokens[4], tokens[6], tokens[3]);
